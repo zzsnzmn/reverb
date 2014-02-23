2c27247ca0c82fbc349a8781c4d3d7e3a2c18cb6
david <zzsnzmn@users.noreply.github.com>
2014-02-23 16:35:34 -0600

    updated tests and made Person more skeleton-y

--- person.rb
+++ person.rb
@@ -1,2 +1,6 @@
 class Person
+    attr_accessor :last_name, :first_name, :gender, :favorite_color, :date_of_birth
+
+    def initialize(last_name, first_name, gender, favorite_color, date_of_birth)
+    end
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,19 +1,27 @@
 require_relative "../person"
 
 describe Person do
+    before :each do
+        @person = Person.new("Sutton", "David", "male", "cerulean", "01/18/1990")
+    end
 
     it "has a last_name" do
+        @person.last_name.should eql "Sutton"
     end
 
     it "has a first_name" do
+        @person.first_name.should eql "David"
     end
 
     it "has a gender" do
+        @person.gender.should eql "male"
     end
 
     it "has a favorite_color" do
+        @person.gender.should eql "cerulean"
     end
 
     it "has a date_of_birth" do
+        @person.date_of_birth.should eql "01/18/1990"
     end
 end

f661ca286b0d5ec63767d27f68b782c35ded6c8b
david <zzsnzmn@users.noreply.github.com>
2014-02-23 16:17:34 -0600

    added person spec, person file and Gemfile

---
+++ spec/person_spec.rb
@@ -0,0 +1,19 @@
+require_relative "../person"
+
+describe Person do
+
+    it "has a last_name" do
+    end
+
+    it "has a first_name" do
+    end
+
+    it "has a gender" do
+    end
+
+    it "has a favorite_color" do
+    end
+
+    it "has a date_of_birth" do
+    end
+end

3cc1b3349b352bce1c4a62beb6905340242ce88d
david <zzsnzmn@users.noreply.github.com>
2014-02-24 19:16:00 -0600

    made the printer_spec tests 'more readable'

--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -19,18 +19,16 @@ describe Printer do
             @printer.files.should eql ["../data/david.csv"]
         end
         
-        it "adds people from all files" do
+        it "adds people from a file" do
             @printer = Printer.new(["../data/david.csv"])
-            @person = Person.new("Sutton", "David", "male", "cerulean", "01/18/1990")
-            @printer.people[0].to_s.should eql @person.to_s
+            @printer.people[0].to_s.should eql "Sutton David male 01/18/1990 cerulean"
             @printer.people.size.should eql 1
         end
 
-        it "adds people from all files" do
+        it "handles more than one file" do
             @printer = Printer.new(["../data/david.csv", "../data/lindsey.csv"])
-            @person = Person.new("Brockman", "Lindsey", "female", "blue", "08/18/1990")
-            @printer.people[0].to_s.should eql @person.to_s
-            @printer.people[1].to_s.should eql @person.to_s
+            @printer.people[0].to_s.should eql "Sutton David male 01/18/1990 cerulean"
+            @printer.people[1].to_s.should eql "Brockman Lindsey female 08/18/1990 blue"
             @printer.people.size.should eql 2
         end
     end
@@ -53,18 +51,15 @@ describe Printer do
     describe "#read_file" do
         it "adds Person objects from a given filename" do
             @printer = Printer.new([])
-            @person = Person.new("Sutton", "David", "male", "cerulean", "01/18/1990")
-            @printer.read_file("../data/david.csv")[0].to_s.should eql @person.to_s
+            @printer.read_file("../data/david.csv")[0].to_s.should eql "Sutton David male 01/18/1990 cerulean"
         end
     end
     
     describe "#read_files" do
         it "returns a list of people" do
             @printer = Printer.new(["../data/david.csv", "../data/lindsey.csv"])
-            @person0 = Person.new("Sutton", "David", "male", "cerulean", "01/18/1990")
-            @person1 = Person.new("Brockman", "Lindsey", "female", "blue", "08/18/1990")
-            @printer.people[0].to_s.should eql @person0.to_s
-            @printer.people[1].to_s.should eql @person1.to_s
+            @printer.people[0].to_s.should eql "Sutton David male 01/18/1990 cerulean"
+            @printer.people[1].to_s.should eql "Brockman Lindsey female 08/18/1990 blue"
             @printer.people.size.should eql 2
         end
     end
@@ -72,15 +67,12 @@ describe Printer do
     describe "#get_by_gender" do
         it "returns a list of people sorted by gender" do
             @printer = Printer.new(["../data/david.csv", "../data/lindsey.csv", "../data/batman.csv", "../data/snoozemen.csv"])
-            @person0 = Person.new("Sutton", "David", "male", "cerulean", "01/18/1990")
-            @person1 = Person.new("Brockman", "Lindsey", "female", "blue", "08/18/1990")
             @person2 = Person.new("Wayne", "Batman", "male", "black", "04/07/1915")
-            @person3 = Person.new("Snoozemen", "Zz", "male", "green", "01/01/1942")
             @list = @printer.get_by_gender
-            @list[0].to_s.should eql @person1.to_s
-            @list[1].to_s.should eql @person3.to_s
-            @list[2].to_s.should eql @person0.to_s
-            @list[3].to_s.should eql @person2.to_s
+            @list[0].to_s.should eql "Brockman Lindsey female 08/18/1990 blue"
+            @list[1].to_s.should eql "Snoozemen Zz male 01/01/1942 green"
+            @list[2].to_s.should eql "Sutton David male 01/18/1990 cerulean"
+            @list[3].to_s.should eql "Wayne Batman male 04/07/1915 black"
             @list.size.should eql 4
         end
     end
@@ -88,15 +80,11 @@ describe Printer do
     describe "#get_by_dob" do
         it "returns a list of people sorted by birthday (oldest first)" do
             @printer = Printer.new(["../data/david.csv", "../data/lindsey.csv", "../data/batman.csv", "../data/snoozemen.csv"])
-            @person0 = Person.new("Sutton", "David", "male", "cerulean", "01/18/1990")
-            @person1 = Person.new("Brockman", "Lindsey", "female", "blue", "08/18/1990")
-            @person2 = Person.new("Wayne", "Batman", "male", "black", "04/07/1915")
-            @person3 = Person.new("Snoozemen", "Zz", "male", "green", "01/01/1942")
             @list = @printer.get_by_dob
-            @list[0].to_s.should eql @person2.to_s
-            @list[1].to_s.should eql @person3.to_s
-            @list[2].to_s.should eql @person0.to_s
-            @list[3].to_s.should eql @person1.to_s
+            @list[0].to_s.should eql "Wayne Batman male 04/07/1915 black"
+            @list[1].to_s.should eql "Snoozemen Zz male 01/01/1942 green"
+            @list[2].to_s.should eql "Sutton David male 01/18/1990 cerulean"
+            @list[3].to_s.should eql "Brockman Lindsey female 08/18/1990 blue"
             @list.size.should eql 4
         end
     end
@@ -104,13 +92,10 @@ describe Printer do
     describe "#get_by_name" do
         it "returns a list of people sorted by last_name Z-A" do
             @printer = Printer.new(["../data/david.csv", "../data/lindsey.csv", "../data/batman.csv"])
-            @person0 = Person.new("Sutton", "David", "male", "cerulean", "01/18/1990")
-            @person1 = Person.new("Brockman", "Lindsey", "female", "blue", "08/18/1990")
-            @person2 = Person.new("Wayne", "Batman", "male", "black", "04/07/1915")
             @list = @printer.get_by_name
-            @list[0].to_s.should eql @person2.to_s
-            @list[1].to_s.should eql @person0.to_s
-            @list[2].to_s.should eql @person1.to_s
+            @list[0].to_s.should eql "Wayne Batman male 04/07/1915 black"
+            @list[1].to_s.should eql "Sutton David male 01/18/1990 cerulean"
+            @list[2].to_s.should eql "Brockman Lindsey female 08/18/1990 blue"
             @list.size.should eql 3
         end
     end
